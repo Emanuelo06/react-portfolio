f1c0ac94fd7c9a5001cc0449edb760b1294cded3
Emanuele Czofei <emanuelecz06@gmail.com>
2025-04-25 09:41:34 +0200

    Responsive Projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
+import React from 'react';
 
-
-const Project = ({imgUrl, title, description, projectUrl, skills}) => {
+const Project = ({ imgUrl, title, description, projectUrl, skills }) => {
   return (
-    <div className='w-full bg-[#3d3d3d] rounded-[8px] p-4 mb-4 flex  '>
-      <img src={imgUrl} alt="project image" 
-      className='w-[150px] md:w-[200px] rounded-[8px] cursor-pointer'/>
-      <div className='h-full self-start self-row'>
-         <h1 className='text-lg md:text-2xl text-white ml-2 underline'><a href={projectUrl} target='blank'>{title}</a></h1>
-         <p className='text-md md:text-[17px] text-white ml-2 md:mb-3 '><a href="https://react-todo-tan-five.vercel.app" target='blank'></a>{description}</p>
-         {skills.map((skill, index) => (
-             <span
-             key={index}
-             className='bg-[#1f1f1f] text-white text-xs md:text-[16px] px-3 py-1 rounded-full align-bottom ml-2'
-           >
-             {skill}
-           </span>
-         ))}
+    <div className="w-full bg-[#3d3d3d] rounded-[8px] p-4 mb-4 flex flex-col sm:flex-row">
+      {/* Image */}
+      <img
+        src={imgUrl}
+        alt="project image"
+        className="sm:w-[150px] md:w-[200px]  rounded-[8px] cursor-pointer mb-4 sm:mb-0 sm:mr-4 self-center"
+      />
+
+      {/* Text content */}
+      <div className="flex flex-col justify-start">
+        <h1 className="text-lg md:text-2xl text-white underline mb-2">
+          <a href={projectUrl} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h1>
+        <p className="text-md md:text-[17px] text-white mb-2">{description}</p>
+        
+        {/* Skills */}
+        <div className="flex flex-wrap">
+          {skills.map((skill, index) => (
+            <span
+              key={index}
+              className="bg-[#1f1f1f] text-white text-xs md:text-sm px-3 py-1 rounded-full mr-2 mb-2"
+            >
+              {skill}
+            </span>
+          ))}
+        </div>
       </div>
-      
-      
-
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
